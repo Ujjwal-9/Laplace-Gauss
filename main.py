f01a3f0ce8384a21c399a1c99ac45c94ca6335d1
Ujjwal <ujjwalupadhyay8@gmail.com>
2018-04-21 20:28:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-
+# Libraries
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,33 +9,33 @@ import numpy as np
 import cv2
 
 
-
+# Load Image
 img = cv2.imread('test.jpg', 0)
 img.shape
 
 
-
+# Plot
 def plot(img):
     fig = plt.imshow(img, cmap = 'gray')
     fig.axes.get_xaxis().set_visible(False)
     fig.axes.get_yaxis().set_visible(False)
 
 
-
+# Load image in Tensor
 img = torch.Tensor(img)
 img = (img.unsqueeze(0)).unsqueeze(0)
 img.shape
 
 
 
-
+# define a kernel
 kernel = torch.Tensor([[[[1,4,6,4,1],[4,16,24,16,4],[6,24,36,24,6],[4,16,24,16,4],[1,4,6,4,1]]]])
 kernel = kernel/torch.sum(kernel)
 kernel.shape
 
 
 
-
+# variables
 src = autograd.Variable(img)
 kernel = autograd.Variable(kernel)
 pyramid_size = 7
@@ -47,7 +46,7 @@ print("src shape: ", src.size())
 print("kernel shape : ", kernel.size())
 
 
-
+# padding function to calculate padding
 def pad(input, padding, kind, k_h, k_w, s_h=1, s_w=1, dilation=1):
     if padding == 'VALID':
         return input
@@ -66,7 +65,7 @@ def pad(input, padding, kind, k_h, k_w, s_h=1, s_w=1, dilation=1):
         return input
 
 
-
+# Gaussian Blur
 out = []
 out.append(src)
 pool = nn.AvgPool2d(kernel_size=(3,3), stride=(2,2))
@@ -76,24 +75,19 @@ for i in range(pyramid_size):
     out.append(src)
 
 
-
+# function to make tensor suitable for plotting
 def for_image(i, out):
-    test =  out[i].view(-1, out[i].size()[-1])
+    test =  out[i].view(-1, out[i].size(-1))
     test = test.data.numpy()
     return test
 
 
-
-def image(img):
-    img = img.view(-1, img.size(-1))
-    return img.data.numpy()
-
-
-
+# plotting 3rd image in gaussian pyramid
 plot(for_image(3, out))
 
 
 
+# Laplacian pyramid
 l_out = []
 for i in range(1,pyramid_size-1):
     ops = F.conv2d(out[i+1], kernel)
@@ -103,5 +97,5 @@ for i in range(1,pyramid_size-1):
     l_out.append(L)
 
 
-
+# plotting 3rd image in Laplacian pyramid
 plot(for_image(3, l_out))
